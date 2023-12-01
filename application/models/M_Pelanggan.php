016cd7fca49326631137eeb60f4a06fcc3162e75
Dimas Bagus Wahyu Sudrajat <47965622+dimas150798@users.noreply.github.com>
2023-12-01 16:13:22 +0700

    Merubah Kode Name PPPOE DRG

--- application/models/M_Pelanggan.php
+++ application/models/M_Pelanggan.php
@@ -231,7 +231,7 @@ class M_Pelanggan extends CI_Model
             $dataRowExistingCode = $queryExistingCode->row();
             $existingCode = $dataRowExistingCode->existingCode;
         } else {
-            $existingCode = "kng0000"; // Default value jika tidak ada data
+            $existingCode = "drg0000"; // Default value jika tidak ada data
         }
 
         // Mengambil angka dari kode yang diperoleh dari database
@@ -253,7 +253,7 @@ class M_Pelanggan extends CI_Model
 
         $no = sprintf("%'.04d", $dataN);
 
-        $invoice = "kng" . $no;
+        $invoice = "drg" . $no;
         return $invoice;
     }
 
